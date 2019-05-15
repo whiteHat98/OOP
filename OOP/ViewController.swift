49b6c8eab48d01f9740e1d77688d0198897db64b
White <randy.noel@binus.ac.id>
2019-05-15 08:14:13 +0700

    Initial Commit

---
+++ OOP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OOP
+//
+//  Created by Randy Noel on 15/05/19.
+//  Copyright © 2019 whiteHat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
